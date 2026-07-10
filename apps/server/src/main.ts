c1b555390428f7b0cdd0c6efdbf291856d89c75c
Nguyen Hieu <hieunt270702@gmail.com>
2026-07-10 16:02:24 +0700

    fix(deploy): bind nestjs to 0.0.0.0 and update default start script to start backend

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
